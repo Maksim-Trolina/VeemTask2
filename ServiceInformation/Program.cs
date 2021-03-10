3e02ed708cab201194a3309d57a9d3438c90f20e
Maksim <maks.panasenko01@bk.ru>
2021-03-10 21:54:24 +0300

    Init

---
+++ ServiceInformation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
